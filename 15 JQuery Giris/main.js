40e98f9e31b20694d5640ea1aa97c1a1bac35530
Mustafa Şenel <m_senell@hotmail.com>
2017-07-29 16:46:35 +0300

    Butona Göster/Gizle özelliği eklendi

--- 15 JQuery Giris/main.js
+++ 15 JQuery Giris/main.js
@@ -7,5 +7,25 @@ var divSecret = $("#divSecret");
 //butona event eklenir:
 btnShow.on("click", function(){
 	//butona tıklandığında div görünür yapılır:
-	divSecret.show();
+	//divSecret.show();
+
+	//Butona hem göster hem de gizle özelliği vermek için:
+	//1. yöntem:
+	//divSecret.toggle();
+
+	//2. yöntem:
+	//Div'in css display property'si none ise görünür yapacağız:
+	if(divSecret.css("display") == "none")
+	{
+		divSecret.show();
+		//Burada buton text'ini de Gizle olarak değiştirelim:
+		btnShow.text("Gizle");
+	}
+	//Div'in css property'si none değilse gizleyeceğiz:
+	else
+	{
+		divSecret.hide();
+		//Burada buton text'ini de Göster olarak değiştirelim:
+		btnShow.text("Göster");
+	}
 });
